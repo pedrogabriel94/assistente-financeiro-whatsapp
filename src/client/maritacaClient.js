103e31b320e68382ac36b1dfc4bb672953120015
Pedro Gabriel <pedrodimas94@gmail.com>
2025-01-26 16:35:30 -0300

    ajuste prompt

--- src/client/maritacaClient.js
+++ src/client/maritacaClient.js
@@ -1,5 +1,6 @@
 
 require('dotenv').config();
+var prompt = require('./../db/prompt.json');
 const MARITACA_TOKEN = process.env.MARITACA_TOKEN;
 
 async function classificacao(message) {
@@ -14,7 +15,7 @@ async function classificacao(message) {
         });
 
         const data = await response.json();
-        return JSON.parse(data.choices[0].message.content);
+        return JSON.parse(cleanJsonString(data.choices[0].message.content));
     } catch (error) {
         console.error('Erro ao classificar a mensagem:', error);
         throw new Error('Erro ao processar a classificação');
@@ -27,7 +28,7 @@ function mountObjetoRequestMaritaca(message) {
         "messages": [
             {
                 "role": "system",
-                "content": "Você é um assistente financeiro especializado em extrair informações de uma frase. Forneça um objeto com os seguintes atributos: 'Valor', 'Categoria', 'Tipo' (Despesa/Receita). Inclua também os atributos 'registrar' (true ou false) e 'mensagem'. A resposta deve ser uma string com o objeto, sem a palavra 'json' e sem quebras de linha. Se a frase contiver uma transação válida, o atributo 'registrar' deve ser true, e a 'mensagem' deve ser uma resposta atenciosa ao cliente, confirmando o registro. Se não for uma transação válida, 'registrar' deve ser false e a 'mensagem' deve ser algo relacionado à resposta. Sempre deve ser um objeto com essas respostas, quero que na mensagem, tenha a variavel {nome}, para que eu possa susbtituir e colocar o nome do cliente. ignore as saudaçõs"
+                "content": prompt.prompt
             },
             {
                 "role": "user",
@@ -35,6 +36,13 @@ function mountObjetoRequestMaritaca(message) {
             }
         ]
     }
+}
+
+function cleanJsonString(str) {
+    // Passo 1: Remover as crases
+    let cleanedString = str.replace(/`/g, '');
+
+    return cleanedString;
 }
 
 module.exports = {
