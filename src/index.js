120e3d1ee9d306a24e9cb9141f6c2bdc403ebffa
Pedro Gabriel <pedrodimas94@gmail.com>
2025-01-29 16:45:46 -0300

    Voltando as coisas que esse baitola "voltou"

--- src/index.js
+++ src/index.js
@@ -9,52 +9,9 @@ app.use(express.json({limit: '5mb'}));
 
 app.use('/', require('./route/generalRouter'));
 
-app.post('/send-message', (req, res) => {
-  sendMessage(req.body);
-  res.send('Mensagem enviada com sucesso!');
-});
-
 app.get('/users', async (req, res) => {
   let users = await db.select('*').from('users');
   res.json(users);
-});
-
-app.post('/recive-message', (req, res) => {
-  console.log(req.body);
-  let data = req.body;
-  let usuario = usuarios.find(user => user.phone === data.phone);
-  if(!usuario){
-    usuarios.push({
-      senderName: data.senderName,
-      phone: data.phone,
-      numberOfMessages: 0
-    });
-    usuario = usuarios.find(user => user.phone === data.phone);
-  } 
-  if(usuario.numberOfMessages === 0) {
-    let message = messages.boasVindas.replace('{{nome}}', data.senderName);
-    sendMessage({
-      phone: data.phone,
-      message: message
-    });
-    usuario.numberOfMessages++;
-  } else if(usuario.numberOfMessages === 1) {
-    let classificacao = natural.classificar(data.text.message);
-    let message = messages.registro.replace('{{tipo}}', classificacao.tipo).replace('{{valor}}', classificacao.valor);
-    sendMessage({
-      phone: data.phone,
-      message: message
-    });
-    usuario.numberOfMessages++;
-  } else if(usuario.numberOfMessages === 2) {
-    let message = messages.confirmacao.replace('{{nome}}', data.senderName);
-    sendMessage({
-      phone: data.phone,
-      message: message
-    });
-    usuario.numberOfMessages = 0;
-  }
-  res.send('Mensagem recebida com sucesso!');
 });
 
 app.get('/integrity', (req, res) => {
